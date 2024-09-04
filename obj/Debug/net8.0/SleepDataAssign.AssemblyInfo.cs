6561a46793843b324e7ec7e4b743735979e61b51
NHeadson <nheadson@my.wctc.edu>
2024-09-04 15:00:18 -0500

    project ready to be started

--- obj/Debug/net8.0/SleepDataAssign.AssemblyInfo.cs
+++ obj/Debug/net8.0/SleepDataAssign.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SleepDataAssign")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e3a81ca68e6ecd7bf9cb6cfac1d8fd4e7dcf54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50586871b7c74687ba295683d0796497ada41899")]
 [assembly: System.Reflection.AssemblyProductAttribute("SleepDataAssign")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SleepDataAssign")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
